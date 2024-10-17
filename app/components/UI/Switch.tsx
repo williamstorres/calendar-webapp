83fd6d5916a069e65059038e7d5d88dede53d5b6
Williams <williams.torres@pm.me>
2024-10-17 17:08:24 -0300

    chore: remove some css animation

--- app/components/UI/Switch.tsx
+++ app/components/UI/Switch.tsx
@@ -39,7 +39,7 @@ export const Switch = forwardRef<HTMLInputElement, SwitchProps>(function Switch(
         ref={ref}
         className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
       />
-      <span className="w-16 h-10 flex items-center flex-shrink-0 ml-4 p-1 bg-primary rounded-full duration-300 ease-in-out peer-checked:bg-blue-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
+      <span className="w-16 h-10 flex items-center flex-shrink-0 ml-4 p-1 bg-primary rounded-full duration-300 ease-in-out peer-checked:bg-blue-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6"></span>
     </label>
   );
 });
